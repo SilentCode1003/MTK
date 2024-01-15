20fcddf9feefed6936a9ac7c7c511effd4e1aed9
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2024-01-15 11:51:12 +0800

    revise read and write of json file

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const String apiUrl = 'http://192.168.30.81:3005/';
+  static const String apiUrl = 'http://192.168.30.18:3005/';
 
   static const String loginAPI = 'login/login';
   static const String leaveAPI = 'eportalrequestleave/getleave';
